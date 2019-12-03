47d289c9105235ce1e11c36146cc25f1209cd80a
Will <will@obo.pm>
2019-12-02 21:52:28 -0800

    initial upload

---
+++ src/lib/styles/theme-base.jsx
@@ -0,0 +1,5 @@
+const transition = {
+  transition: 'all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)',
+}
+
+export { transition }
---
+++ src/view-layer/pages/About/index.jsx
@@ -0,0 +1,6 @@
+  
+import React from 'react';
+
+const About = () => <h3>About.</h3>
+
+export default About;
